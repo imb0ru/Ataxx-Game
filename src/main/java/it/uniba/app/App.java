3625857121fac4a9b9592a8e4884dc3aa5cf1f86
Marco Ferrara <marcoferrara1510@gmail.com>
2024-05-08 16:32:59 +0200

    Creata Classe AppController
    Classe AppController che si occupa di gestire la prima esecuzione del programma con o senza argomenti e verrà implementata con l'aggiunta della gestione di ogni comando in base all'input dell'utente

--- src/main/java/it/uniba/app/App.java
+++ src/main/java/it/uniba/app/App.java
@@ -20,6 +20,6 @@ public final class App {
      * @param args command line arguments
      */
     public static void main(final String[] args) {
-        System.out.println(new App().getGreeting());
+        new AppController(args);
     }
 }
