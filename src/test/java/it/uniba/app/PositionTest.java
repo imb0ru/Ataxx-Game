532ef13154bc3f9c6702491e86f3b434feb41bc0
iamariateresa7 <marydichio03@gmail.com>
2024-06-08 19:34:50 +0200

    PositionTest: modificata le classe per rispettare le regole di CheckStyle

--- src/test/java/it/uniba/app/PositionTest.java
+++ src/test/java/it/uniba/app/PositionTest.java
@@ -4,7 +4,10 @@ import it.uniba.app.entities.Board;
 import it.uniba.app.exceptions.InvalidPositionException;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * Test per la classe {@link it.uniba.app.entities.Board.Position}.
@@ -15,8 +18,11 @@ class PositionTest {
      */
     @Test
     void constructorTest() {
-        var position = new Board.Position(0, 3);
-        assertTrue(position.row() == 0 && position.column() == 3, "La riga dovrebbe avere indice 0 e la colonna 3");
+        final var row = 0;
+        final var column = 3;
+        var position = new Board.Position(row, column);
+        assertTrue(position.row() == row && position.column() == column,
+            "La riga dovrebbe avere indice 0 e la colonna 3");
     }
 
     /**
@@ -25,7 +31,10 @@ class PositionTest {
      */
     @Test
     void constructorThrowsTest() {
-        assertThrows(InvalidPositionException.class, () -> new Board.Position(10, 3), "La riga non dovrebbe essere valida");
+        final var row = 10;
+        final var column = 3;
+        assertThrows(InvalidPositionException.class, () -> new Board.Position(row, column),
+            "La riga non dovrebbe essere valida");
     }
 
     /**
@@ -34,8 +43,11 @@ class PositionTest {
      */
     @Test
     void fromStringTest() {
+        final var row = 0;
+        final var column = 3;
         var position = Board.Position.fromString("a3");
-        assertTrue(position.row() == 0 && position.column() == 3,"La posizione dovrebbe essere a3");
+        assertTrue(position.row() == row && position.column() == column,
+            "La posizione dovrebbe essere a3");
     }
 
     /**
@@ -45,7 +57,8 @@ class PositionTest {
      */
     @Test
     void fromStringThrowsTest() {
-        assertThrows(InvalidPositionException.class, () -> Board.Position.fromString("h9"), "La posizione non dovrebbe essere valida");
+        assertThrows(InvalidPositionException.class, () -> Board.Position.fromString("h9"),
+            "La posizione non dovrebbe essere valida");
     }
 
     /**
@@ -55,10 +68,29 @@ class PositionTest {
     @Test
     void distanceTest() {
         assertAll(() -> {
-            assertEquals(2, Board.Position.distance(Board.Position.fromString("a3"), Board.Position.fromString("a1")), "La distanza dovrebbe essere 2");
-            assertEquals(0, Board.Position.distance(Board.Position.fromString("c7"), Board.Position.fromString("c7")), "La distanza dovrebbe essere 0");
-            assertEquals(5, Board.Position.distance(Board.Position.fromString("f7"), Board.Position.fromString("f2")), "La distanza dovrebbe essere 5");
-            assertEquals(3, Board.Position.distance(Board.Position.fromString("g6"), Board.Position.fromString("g3")), "La distanza dovrebbe essere 3");
+            assertEquals(2, Board.Position.distance(
+                    Board.Position.fromString("a3"),
+                    Board.Position.fromString("a1")
+                ),
+                "La distanza dovrebbe essere 2");
+
+            assertEquals(0, Board.Position.distance(
+                    Board.Position.fromString("c7"),
+                    Board.Position.fromString("c7")
+                ),
+                "La distanza dovrebbe essere 0");
+
+            assertEquals(5, Board.Position.distance(
+                    Board.Position.fromString("f7"),
+                    Board.Position.fromString("f2")
+                ),
+                "La distanza dovrebbe essere 5");
+
+            assertEquals(3, Board.Position.distance(
+                    Board.Position.fromString("g6"),
+                    Board.Position.fromString("g3")
+                ),
+                "La distanza dovrebbe essere 3");
         });
     }
 }
